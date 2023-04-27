87ec702e38727c5dab50e894d47cdea5c9a1cbbe
Leonardo <diogoleo738@outlook.com>
2023-04-27 11:38:14 -0300

    first commit

---
+++ quiz.py
@@ -0,0 +1,37 @@
+
+print("Seja muito bem vindo ao quiz")
+answer_user = input("Quer começar? (S/N) ")
+
+if answer_user != "S":
+    quit()
+
+print("Começando...")
+print("pergunta numero 1")
+print("""Qual foi a primeira linguagem de programação?
+\n (A) Assembly \n (B) Fortran \n (C) Lisp \n (D) Cobol""")
+
+answer_1 = input("Resposta: ")
+
+if answer_1 == "B":
+    print("Correto!")
+else:
+    print("Incorreto")
+
+continuar = input("Deseja continuar? (S/N) ")
+
+if continuar == "S":
+    print("Continuando")
+else:
+    quit()
+
+print("pergunta 2")
+
+print("""qual a linguagem mais famosa da atualidade?
+\n (A) JavaScript \n (B) Python \n (C) Java \n (D) C# """)
+
+answer_2 = input("Resposta")
+
+if answer_2 == "B":
+    print("Correto")
+else:
+    print("Incorreto")
